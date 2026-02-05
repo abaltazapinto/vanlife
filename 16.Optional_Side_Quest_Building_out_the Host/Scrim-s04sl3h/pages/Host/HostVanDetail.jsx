77dcc64123893d9d89d30972e5eebf69aa3c7e48
abaltaza <146941254+abaltazapinto@users.noreply.github.com>
2026-02-05 12:14:15 +0000

    Implement host vans list and host van detail pages

--- 16.Optional_Side_Quest_Building_out_the Host/Scrim-s04sl3h/pages/Host/HostVanDetail.jsx
+++ 16.Optional_Side_Quest_Building_out_the Host/Scrim-s04sl3h/pages/Host/HostVanDetail.jsx
@@ -1,5 +1,41 @@
 import React from "react"
+import { Link, useParams } from "react-router-dom"
 
 export default function HostVanDetail() {
-    return <h1>Host Van Detail Page</h1>
+    const params = useParams()
+    const [van, setVan] = React.useState(null)
+
+    React.useEffect(() => {
+        fetch(`/api/host/vans/${params.id}`)
+            .then(res => res.json())
+            .then(data => setVan(data.vans[0]))
+    }, [params.id])
+
+    return (
+        <section className="host-van-detail-section">
+            <Link to=".." relative="path" className="back-button">
+                &larr; <span>Back to all vans</span>
+            </Link>
+
+            {van ? (
+                <div className="host-van-detail-layout-container">
+                    <div className="host-van-detail">
+                        <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+                        <div className="host-van-detail-info-text">
+                            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                            <h3>{van.name}</h3>
+                            <h4>${van.price}/day</h4>
+                        </div>
+                    </div>
+
+                    <div className="host-van-detail-info">
+                        <p><span>Name:</span> {van.name}</p>
+                        <p><span>Category:</span> {van.type}</p>
+                        <p><span>Description:</span> {van.description}</p>
+                        <p><span>Visibility:</span> Public</p>
+                    </div>
+                </div>
+            ) : <h2>Loading...</h2>}
+        </section>
+    )
 }
