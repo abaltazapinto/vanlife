4c8547400026641f2f59b91cedc79e893f2972ce
abaltazapinto <andrebaltazarpinto@gmail.com>
2026-02-09 09:24:41 +0000

    search params init

--- 18.OutletContext_exercise1/My solution/pages/Host/HostVanInfo.jsx
+++ 18.OutletContext_exercise1/My solution/pages/Host/HostVanInfo.jsx
@@ -4,9 +4,8 @@ import { useOutletContext } from "react-router-dom"
 export default function HostVanInfo() {
     const { currentVan } = useOutletContext()
     console.log("outletContext:", useOutletContext)
-    
     console.log("currentVan:", currentVan)
-    return ( 
+    return (
         <h2>{currentVan.description} Detailed info goes here</h2>
     )
 }
